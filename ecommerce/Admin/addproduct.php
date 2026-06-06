5e6045963d980fa8fd2b63612ea02c80bbc588be
harisnaseer24 <harisnaseer258@gmail.com>
2026-06-06 18:04:59 +0500

    file upload done

---
+++ ecommerce/Admin/addproduct.php
@@ -0,0 +1,154 @@
+<?php 
+@include_once('./components/header.php');
+@require_once('../config/connection.php');
+?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <h3 class="page-title">
+              Add Product
+            </h3>
+          </div>
+         <div class="row">
+
+ <div class="col-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                 
+                  <form class="forms-sample" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                      <label for="title">Product Title</label>
+                      <input type="text" name="title" required class="form-control" id="exampleInputName1" placeholder="Title">
+                    </div>
+                    <div class="form-group">
+                      <label for="price">Price</label>
+                      <input type="number" required  name="price" class="form-control" id="price" placeholder="Price in PKR">
+                    </div>
+                    <div class="form-group">
+                      <label for="stock">Stock</label>
+                      <input type="number" required  name="stock" class="form-control" id="stock" placeholder="Stock">
+                    </div>
+                  
+                    <div class="form-group">
+                      <label for="cat_id">Categories</label>
+                        <select class="form-control" id="cat_id" name="cat_id">
+                          <option value="1">Electronics</option>
+                          
+                        </select>
+                      </div>
+                    <div class="form-group">
+                      <label>File upload</label>
+                      <input type="file" name="img" class="file-upload-default">
+                      <div class="input-group col-xs-12">
+                        <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                        <span class="input-group-append">
+                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                        </span>
+                      </div>
+                    </div>
+                   
+                    <div class="form-group">
+                      <label for="description">Description</label>
+                      <textarea class="form-control" name="description" id="description" rows="4"></textarea>
+                    </div>
+                    <button type="submit" name="add" class="btn btn-primary mr-2">Submit</button>
+                    <button class="btn btn-light">Cancel</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+         </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="./vendors/js/vendor.bundle.base.js"></script>
+  <script src="./vendors/js/vendor.bundle.addons.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="./js/off-canvas.js"></script>
+  <script src="./js/hoverable-collapse.js"></script>
+  <script src="./js/misc.js"></script>
+  <script src="./js/settings.js"></script>
+  <script src="./js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="./js/file-upload.js"></script>
+  <script src="./js/typeahead.js"></script>
+  <script src="./js/select2.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+
+</html>
+<?php 
+
+if(isset($_POST['add'])){
+
+$title = $_POST['title'];
+$price = $_POST['price'];
+$stock = $_POST['stock'];
+$description = $_POST['description'];
+$cat_id = $_POST['cat_id'];
+// file handling
+if($_FILES['img']['error']==4){
+  echo "<script>alert('No Image Uploaded');</script>";
+}else if($_FILES['img']['size'] > 2000000){
+  echo "<script>alert('Files size must be under 2MBs');</script>";
+}else{
+$imgname=$_FILES['img']['name'];
+$tmpname=$_FILES['img']['tmp_name'];
+
+$validExtensions=["png","jpg","jpeg","gif","webp","jfif"];
+// abc.jpg
+// harisnaseer= ["haris","naseer"]
+// abc.kavish.latest.png
+$extension= explode(".",$imgname);// ["kaavish", "PNG"]
+// print_r($extension);
+$extension= strtolower(end($extension));//png
+if(!in_array($extension, $validExtensions)){
+  echo "<script>alert('Invalid File Type');</script>";
+
+}
+
+$newimgname=uniqid().".".$extension;//4545gh45rt454242.jpg
+// $newimgname=$imgname;//kavish.png
+$insertQuery = "INSERT INTO `products`( `title`, `description`, `price`, `stock`, `image`, `cat_id`) VALUES ('$title','$description','$price','$stock','$newimgname','$cat_id')";
+
+$result= mysqli_query($conn,$insertQuery);
+if($result){
+
+move_uploaded_file($tmpname,"./uploads/".$newimgname);
+  echo "<script>alert('Product Added Successfully');</script>";
+
+}else{
+    echo "<script>alert('Error Adding Product');</script>";
+}
+
+}
+
+}
+
+?>
---
+++ ecommerce/config/connection.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+$server="localhost";
+$username="root";
+$password="";
+$dbname="2509b1_ecommerce";
+
+
+$conn = mysqli_connect($server, $username, $password,$dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
