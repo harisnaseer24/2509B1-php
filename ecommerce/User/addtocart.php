17f16cbd487c917d64bda9ac0bc8611601df9b54
harisnaseer24 <harisnaseer258@gmail.com>
2026-07-15 10:42:45 +0500

    cart duplicacy removal

--- ecommerce/User/addtocart.php
+++ ecommerce/User/addtocart.php
@@ -13,7 +13,35 @@ if(isset($_POST['addtocart'])){
   $qty = $_POST['qty'];
   $user_id = $_SESSION['user_id'];
 
-  $total=$price * $qty;
+$checkDuplicateQuery="SELECT * FROM  cart where u_id=$user_id and p_id=$p_id";
+$checkDuplicateQueryResult=mysqli_query($conn,$checkDuplicateQuery);
+if(mysqli_num_rows($checkDuplicateQueryResult) > 0){
+
+echo "Updating qty";
+
+$row=mysqli_fetch_assoc($checkDuplicateQueryResult);
+
+$cartid= $row["cart_id"];
+$oldQty= $row["qty"];
+          // 3    + 3 = 6
+$finalQty = $oldQty + $qty;
+
+//       price * 6
+ $total=$price * $finalQty;
+  $updateCartQuery = "Update `cart` set  `qty`= $finalQty,`total`= $total Where cart_id = $cartid";
+  $updateCartQueryResult = mysqli_query($conn,$updateCartQuery);
+
+  if($updateCartQueryResult){
+    header("Location:./cart.php");
+  }
+  else{
+    echo "Error: ".mysqli_error($conn);
+  }
+
+}else{
+echo "Adding product";
+
+ $total=$price * $qty;
 
   $insertCartQuery = "INSERT INTO `cart`(`u_id`, `p_id`, `price`, `qty`,`total`) VALUES ($user_id,$p_id,$price,$qty,$total)";
   $insertCartQueryResult = mysqli_query($conn,$insertCartQuery);
@@ -24,6 +52,14 @@ if(isset($_POST['addtocart'])){
   else{
     echo "Error: ".mysqli_error($conn);
   }
+
+}
+
+
+
+
+
+ 
 }
 
 
